a4680ea93a8bd0ade2764b10f37dc1a09390996d
Serime <nicolas.gaillard09@gmail.com>
2024-01-02 05:26:11 +0100

    Ajout Footer avec form Contact

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import './App.css';
 import './SectionTechDoukro.scss';
+import './SectionIntroduceDoukro.scss';
+import './SectionKasa.scss';
+import './SectionScreenshot.scss';
+import './Header.scss';
+import Footer from './components/Footer/Footer';
 
-import styled from "styled-components";
-
-const SectionTechDoukro = styled.section`
-`;
-
+// <a href='https://serime.github.io/' target="_blank" rel="noreferrer">Lien du site</a>
 
 function App() {
   return (
     <div className="App">
-      <header></header>
-      <SectionTechDoukro className='SectionTechDoukro'>
+      <header>
+        <div className='gradient-border'>
+          <div>
+            <h1>Nicolas Gaillard-Melac</h1>
+            <span>Développeur Web</span>
+          </div>
+        </div>
+      </header>
+      <section className='SectionKasa'>
+        <h2>Kasa</h2>
+        <p>Intégration d'une maquette <strong>Figma</strong> avec une <strong>React</strong> tout en étant <strong>responsive</strong> et en faisant attention à l'<strong>accessiblilité</strong></p>
+        <div>
+          <img id='ImageMobile' src='Kasa_Mobile.png' alt="Capture d'écran mobile du site Kasa"></img>
+          <img id='ImageDesktop' src='Kasa_Desktop.png' alt="Capture d'écran du site Kasa"></img>
+        </div>
+        <div className='buttons'>
+          <button>Voir le site</button>
+          <button>
+            <img id='GitHub_Name' src='github-mark.svg' alt="Capture d'écran du site Kasa"></img>
+            <img id='GitHub_Logo' src='GitHub_Logo.png' alt="Capture d'écran du site Kasa"></img>
+          </button>
+        </div>
+      </section>
+      <section className='SectionIntroduceDoukro'>
+        <h2>Mon projet du moment</h2>
+        <span>Doukro</span>
+        <p>Un jeu de carte en <strong>multijoueur temps réel</strong> dont les images des cartes sont généré par <strong>IA</strong></p>
+        <div className='Card_Images'>
+          <img src='Card_Images/Border crossing.png' alt="Carte Border crossing du jeu Doukro"></img>
+          <img src='Card_Images/Former blacksmith.png' alt="Carte Former blacksmith du jeu Doukro"></img>
+          <img src='Card_Images/Knight guarding the entrance to the city.png' alt="Carte Knight guarding the entrance to the city du jeu Doukro"></img>
+          <img src='Card_Images/Luminous deity.png' alt="Carte Luminous deity du jeu Doukro"></img>
+        </div>
+      </section>
+      <section className='SectionTechDoukro'>
         <h3>Architecture global du projet</h3>
+        <p>Une application web <strong>React</strong> avec une <strong>API</strong> pour l'authentification des utilisateurs et les <strong>WebSocket</strong> pour le temps réel</p>
         <img className='LogoTech LogoReact' src='Tech_Logo/React.png' alt='Logo de React'/>
         <img className='LogoTech LogoMongoDB' src='Tech_Logo/MongoDB.png' alt='Logo de MongoDB'/>
         <div className='Docker'>
@@ -28,22 +63,83 @@ function App() {
             <div className='ReactExpressMessageLeft'>
               <span>Réponse</span>
             </div>
-            <div class="arrow ExpressRight"></div>
-            <div class="arrow ExpressLeft"></div>
+            <div className="arrow ExpressRight"></div>
+            <div className="arrow ExpressLeft"></div>
             <div className='ReactSocketIOConnection'></div>
             <div className='ReactSocketIOMessage'>
               <span>Nouveau Message</span>
             </div>
-            <div class="arrow SocketIORight"></div>
-            <div class="arrow SocketIOLeft"></div>
+            <div className="arrow SocketIORight"></div>
+            <div className="arrow SocketIOLeft"></div>
             <img className='LogoExpress' src='Tech_Logo/Express.png' alt='Logo de Express'/>
             <img className='LogoSocketIO' src='Tech_Logo/SocketIO.png' alt='Logo de SocketIO'/>
           </div>
+        </div>        
+      </section>
+      <section className='SectionScreenshot'>
+        <div className="wrapper">
+        <h3>Capture d'écran</h3>
+          <div className="container">
+            <input type="radio" name="slide" id="c1" checked/>
+            <label for="c1" className="card">
+              <div className="row">
+                <img src='Partie en cours.png' alt="Capture d'écran du jeu Doukro"></img>
+                <div className="icon">1</div>
+              </div>
+            </label>
+            <input type="radio" name="slide" id="c2"/>
+            <label for="c2" className="card">
+              <div className="row">
+                <img src='./Card_Images/Former blacksmith.png' alt="Capture d'écran du jeu Doukro"></img>
+                <div className="icon">2</div>
+              </div>
+            </label>
+            <input type="radio" name="slide" id="c3"/>
+            <label for="c3" className="card">
+              <div className="row">
+                <img src='./Card_Images/Knight guarding the entrance to the city.png' alt="Capture d'écran du jeu Doukro"></img>
+                <div className="icon">3</div>
+              </div>
+            </label>            
+            <input type="radio" name="slide" id="c4"/>
+            <label for="c4" className="card">
+              <div className="row">
+                <img src='./Card_Images/Border crossing.png' alt="Capture d'écran du jeu Doukro"></img>
+                <div className="icon">4</div>
+              </div>
+            </label>
+          </div>
         </div>
-      </SectionTechDoukro>
-      <footer></footer>
+        <div className='buttons'>
+          <button>Voir le site</button>
+          <button>
+            <img id='GitHub_Name' src='github-mark.svg' alt="Capture d'écran du site Kasa"></img>
+            <img id='GitHub_Logo' src='GitHub_Logo.png' alt="Capture d'écran du site Kasa"></img>
+          </button>
+        </div>
+      </section>
+      <Footer/>
     </div>
   );
 }
+
+/*
+
+        <form>
+          <div>
+            <label for="nom">Nom :</label>
+            <input type="text" id="nom" name="nom" required/>
+          </div>
+          <div>
+            <label for="email">Adresse e-mail :</label>
+            <input type="email" id="email" name="email" required/>
+          </div>
+          <div>
+            <label for="message">Message :</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+          </div>
+          <button onClick={() => sendMail()}>Envoyer</button>
+        </form>
+*/
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,57 @@
+import './Footer.scss';
+import { Email } from "./smtp";
+import { useState } from 'react';
+
+function sendMail(name, email, message) {
+  Email.send({
+    SecureToken : "9499ea4b-db37-462e-9be1-940269a2bb89",
+    To : "contact@nicolasgaillardmelac.fr",
+    From : "nicolas.gaillard09@gmail.com",
+    Subject : "Contact portfolio",
+    Body : name + '<br>' + email + '<br>' + message
+    }).then( (message) => {
+        if (message === "OK") {
+          alert("Je vous remercie, pour intérêt que vous me portez.\nJe reviens vers vous rapidement.");
+        } else {
+          alert(message);
+        }
+      }
+    );
+}
+
+function Footer() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    sendMail(name, email, message);
+    setName('');
+    setEmail('');
+    setMessage('');
+  }
+
+  return (
+    <footer>
+      <form onSubmit={handleSubmit}>
+        <h3 class="title">Contact</h3>                  
+        <label for="name">
+          <input class="input" type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="" required/>
+          <span>Nom</span>
+        </label>
+        <label for="email">
+            <input class="input" type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="" required/>
+            <span>Email</span>
+        </label>               
+        <label for="message">
+            <textarea class="input" id="message" name="message" rows="4" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
+            <span>Message</span>
+        </label>
+        <button type="submit">Envoyer</button>
+      </form>
+    </footer>
+  );
+}
+
+export default Footer;
